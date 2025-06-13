8a25accc91e8f585a078dc8cdf8f65ec663a5c0c
Jinit Mewada <jinitmewada41@gmail.com>
2025-06-13 19:57:53 +0530

    Link is defined

--- Frontend/src/pages/Login.jsx
+++ Frontend/src/pages/Login.jsx
@@ -4,6 +4,7 @@ import axios from 'axios'
 import { useNavigate } from 'react-router-dom'
 import LogginSuccessModal from './LogginSuccessModal'
 import Car from '../assets/Taycan.png'
+import { Link } from 'react-router-dom'
 
 const Login = () => {
 
